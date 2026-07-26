05c6fbfc60554ffd49459da08d438e1d0ba55d6a
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-07-26 17:05:26 +0200

    Link commit!

---
+++ src/main/kotlin/MyMessageBundle.kt
@@ -0,0 +1,28 @@
+package com.tecknobit.envui
+
+import com.intellij.DynamicBundle
+import org.jetbrains.annotations.Nls
+import org.jetbrains.annotations.PropertyKey
+import java.util.function.Supplier
+
+private const val BUNDLE = "messages.I18nMessageBundle"
+
+internal object MyMessageBundle {
+    private val instance = DynamicBundle(MyMessageBundle::class.java, BUNDLE)
+
+    @JvmStatic
+    fun message(
+        key: @PropertyKey(resourceBundle = BUNDLE) String,
+        vararg params: Any?,
+    ): @Nls String {
+        return instance.getMessage(key, *params)
+    }
+
+    @JvmStatic
+    fun lazyMessage(
+        @PropertyKey(resourceBundle = BUNDLE) key: String,
+        vararg params: Any?,
+    ): Supplier<@Nls String> {
+        return instance.getLazyMessage(key, *params)
+    }
+}
