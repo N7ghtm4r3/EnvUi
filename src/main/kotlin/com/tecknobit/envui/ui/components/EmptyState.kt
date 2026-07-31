34137a08e71263b0e0b4b6101dd70ae3e16243f6
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-07-31 23:35:57 +0200

    feat(EmptyState): introduced component in the flow

---
+++ src/main/kotlin/com/tecknobit/envui/ui/components/EmptyState.kt
@@ -0,0 +1,53 @@
+package com.tecknobit.envui.com.tecknobit.envui.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.intellij.ui.JBColor
+import com.tecknobit.envui.com.tecknobit.envui.ui.utils.toComposeColor
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.jewel.ui.component.Icon
+import org.jetbrains.jewel.ui.component.Text
+import org.jetbrains.jewel.ui.icon.IconKey
+
+@Composable
+fun EmptyState(
+    modifier: Modifier = Modifier,
+    icon: IconKey,
+    iconSize: Dp = 50.dp,
+    title: StringResource,
+    action: @Composable (() -> Unit)? = null,
+) {
+    val text = stringResource(title)
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(iconSize),
+                key = icon,
+                tint = JBColor.namedColor("Component.accentColor").toComposeColor(),
+                contentDescription = text
+            )
+
+            Text(
+                text = text
+            )
+
+            action?.let { it() }
+        }
+    }
+}
--- src/main/kotlin/com/tecknobit/envui/ui/pages/envuiwindow/components/EnvSourcesList.kt
+++ src/main/kotlin/com/tecknobit/envui/ui/pages/envuiwindow/components/EnvSourcesList.kt
@@ -1,37 +1,70 @@
 package com.tecknobit.envui.com.tecknobit.envui.ui.pages.envuiwindow.components
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.tecknobit.envui.com.tecknobit.envui.ui.components.EmptyState
 import com.tecknobit.envui.com.tecknobit.envui.ui.pages.envuiwindow.data.EnvSource
+import com.tecknobit.envui.generated.resources.Res
+import com.tecknobit.envui.generated.resources.envui_no_sources_message
+import org.jetbrains.jewel.ui.component.Text
+import org.jetbrains.jewel.ui.icons.AllIconsKeys
 
 @Composable
 fun EnvSourcesList(
     modifier: Modifier = Modifier,
     sources: List<EnvSource>,
 ) {
-    LazyColumn(
-        modifier = modifier
-            .animateContentSize(),
-        verticalArrangement = Arrangement.spacedBy(10.dp)
-    ) {
-        items(
-            items = sources,
-            key = { envSource -> envSource.path }
-        ) { envSource ->
-            EnvSourceCard(
-                modifier = modifier
-                    .animateItem()
-                    .fillMaxWidth(),
-                envSource = envSource,
-                onClick = {
-                }
+    AnimatedContent(
+        targetState = sources.isEmpty()
+    ) { isEmpty ->
+        if (isEmpty) {
+            NoSourcesAvailable(
+                modifier = Modifier
+                    .fillMaxSize()
             )
+        } else {
+            LazyColumn(
+                modifier = modifier
+                    .animateContentSize(),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                items(
+                    items = sources,
+                    key = { envSource -> envSource.path }
+                ) { envSource ->
+                    EnvSourceCard(
+                        modifier = modifier
+                            .animateItem()
+                            .fillMaxWidth(),
+                        envSource = envSource,
+                        onClick = {
+                        }
+                    )
+                }
+            }
         }
     }
+}
+
+@Composable
+private fun NoSourcesAvailable(
+    modifier: Modifier,
+) {
+    EmptyState(
+        modifier = modifier,
+        icon = AllIconsKeys.Actions.Minimap,
+        iconSize = 75.dp,
+        title = Res.string.envui_no_sources_message,
+        action = {
+            Text("Create new")
+        }
+    )
 }
