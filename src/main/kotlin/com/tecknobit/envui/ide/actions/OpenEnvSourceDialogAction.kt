ffac03af9c5e92cc9ddb0815162d3129517f326c
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-08-14 10:03:27 +0200

    feat(OpenEnvSourceDialogAction) completed the action implementation

--- src/main/kotlin/com/tecknobit/envui/ide/actions/OpenEnvSourceDialogAction.kt
+++ src/main/kotlin/com/tecknobit/envui/ide/actions/OpenEnvSourceDialogAction.kt
@@ -1,12 +1,12 @@
 package com.tecknobit.envui.ide.actions
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.vfs.VirtualFile
 import com.tecknobit.envui.ide.languages.envfile.dEnvFileType
-import com.tecknobit.envui.ide.languages.envfiletemplate.dEnvTemplateFileType
 import com.tecknobit.envui.ui.pages.dialogs.envsourcereader.presenter.EnvSourceReaderDialog
 import com.tecknobit.envui.util.toEnvSource
 
@@ -35,10 +35,14 @@ class OpenEnvSourceDialogAction : AnAction() {
         event.presentation.isEnabled = editorFile != null
     }
 
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.BGT
+    }
+
     private fun AnActionEvent.resolveVirtualFile(): VirtualFile? {
         val editorFile = getData(CommonDataKeys.VIRTUAL_FILE) ?: return null
         val fileType = editorFile.fileType
-        if(fileType != dEnvFileType && fileType != dEnvTemplateFileType)
+        if(fileType != dEnvFileType)
             return null
 
         return editorFile
