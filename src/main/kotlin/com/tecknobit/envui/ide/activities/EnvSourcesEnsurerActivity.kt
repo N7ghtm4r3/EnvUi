10214268f6bec8ad29d5e9b73359f0cb5c9f9ee3
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-09-02 20:52:40 +0200

    feat: implemented the EnvSourcesEnsurerActivity to ensure env file existance when template exists

---
+++ src/main/kotlin/com/tecknobit/envui/ide/activities/EnvSourcesEnsurerActivity.kt
@@ -0,0 +1,44 @@
+package com.tecknobit.envui.ide.activities
+
+import com.intellij.openapi.application.readAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.openapi.vfs.VirtualFile
+import com.tecknobit.envui.ide.languages.envfile.dEnvFile.Companion.ENV_FILENAME
+import com.tecknobit.envui.repositories.EnvSourceRepository
+
+class EnvSourcesEnsurerActivity : ProjectActivity {
+
+    override suspend fun execute(
+        project: Project,
+    ) {
+        val repository = EnvSourceRepository(
+            project = project
+        )
+
+        val templates = repository.retrieveEnvTemplates()
+        templates.forEach { template ->
+            ensureEnvSource(
+                repository = repository,
+                envTemplate = template
+            )
+        }
+    }
+
+    private suspend fun ensureEnvSource(
+        repository: EnvSourceRepository,
+        envTemplate: VirtualFile,
+    ) {
+        val containerDirectory = envTemplate.parent
+        val sourceFileExists = readAction {
+            containerDirectory.findChild(ENV_FILENAME) != null
+        }
+        if (sourceFileExists)
+            return
+
+        repository.createNewEnvSourceFromTemplate(
+            envTemplate = envTemplate
+        )
+    }
+
+}
